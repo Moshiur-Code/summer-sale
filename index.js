18fff6a05212bfac7f4bdc0e7a1bfbe65b7aecbe
Moshiur-Code <moshiurrahman4195@gamil.com>
2023-08-21 17:37:22 +0600

    files added

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('js file added');
+
+function itemCard(target){
+    // console.log(target.childNodes[3].childNodes[3].innerText);
+
+    const itemName = target.childNodes[3].childNodes[3].innerText;
+    console.log(itemName);
+}
