6dd08b9bda08c503226f7b3226fc1b566b810e83
Moshiur <moshiurrahman4195@gmail.com>
2023-08-21 18:33:10 +0600

    item name and price added

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-console.log('js file added');
+// console.log('js file added');
+
+let total = 0;
 
 function itemCard(target){
     // console.log(target.childNodes[3].childNodes[3].innerText);
+    const selectedItemContainer = document.getElementById('selected-item');
 
     const itemName = target.childNodes[3].childNodes[3].innerText;
-    console.log(itemName);
+    // console.log(itemName);
+    const li = document.createElement('li');
+    li.innerText = itemName;
+    console.log(li);
+
+    selectedItemContainer.appendChild(li);
+
+    // console.log(target.childNodes[3].childNodes[5].innerText.split(" ")[0]);
+
+    const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+
+    total = parseFloat(total) + parseFloat(price);
+    
+    console.log(total);
+   document.getElementById('total-price').innerText = total;
+
 }
