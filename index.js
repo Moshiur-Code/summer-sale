a8b35712a29e95323e1c54dd2ba514bd8d14c740
Moshiur <moshiurrahman4195@gmail.com>
2023-08-22 09:23:28 +0600

    files added

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-// console.log('js file added');
 
 let total = 0;
 
 function itemCard(target){
-    // console.log(target.childNodes[3].childNodes[3].innerText);
-    const selectedItemContainer = document.getElementById('selected-item');
+
+    const selectedItemContainer = document.getElementById('order-list');
 
     const itemName = target.childNodes[3].childNodes[3].innerText;
-    // console.log(itemName);
+
+
     const li = document.createElement('li');
     li.innerText = itemName;
     console.log(li);
 
     selectedItemContainer.appendChild(li);
 
-    // console.log(target.childNodes[3].childNodes[5].innerText.split(" ")[0]);
 
     const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
 
     total = parseFloat(total) + parseFloat(price);
-    
-    console.log(total);
+
+
    document.getElementById('total-price').innerText = total;
 
+  let discountAmount = total;
+  let payPrice = total;
+
+  if(discountAmount >= 200) {
+    discountAmount = (discountAmount * 20) / 100;
+    payPrice = total - discountAmount;
+  } else {
+    discountAmount = 0;
+  }
+ 
+  document.getElementById('discount-amount').innerText = discountAmount;
+
+  document.getElementById('pay-total').innerHTML = payPrice;
 }
